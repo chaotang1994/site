69e3b5afabb0b504c34a4cbcf30ab3d786063ae5
Chaoxin Tang <chaotang1994@gmail.com>
2021-04-28 03:07:43 -0400

    adding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  ScrollIntoView(elem: string) {
+    document.querySelector(elem).scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
 
 }
